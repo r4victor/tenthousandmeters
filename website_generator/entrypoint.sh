a5c3296d0f4c76d605dbb8d7beab53fbea9315d4
Victor Skvortsov <vds003@gmail.com>
2021-10-26 11:38:31 +0500

    Add simple logging

--- website_generator/entrypoint.sh
+++ website_generator/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 while true; do
+    echo "Updating links..."
     python links_updater/links_updater.py
+    echo "Running pelican..."
     pelican -e DELETE_OUTPUT_DIRECTORY=''
+    echo "Sleeping..."
     sleep 1h
 done
