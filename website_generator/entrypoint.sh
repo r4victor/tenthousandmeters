a41f1d7d57df2c65d606bbc9914f06e0bd302fe9
Victor Skvortsov <vds003@gmail.com>
2024-05-05 15:55:02 +0500

    Disable links_updater

--- website_generator/entrypoint.sh
+++ website_generator/entrypoint.sh
@@ -3,8 +3,8 @@
 set -e
 
 while true; do
-    echo "Updating links..."
-    python links_updater/links_updater.py
+    # echo "Updating links..."
+    # python links_updater/links_updater.py
     echo "Running pelican..."
     pelican
     echo "Sleeping..."
